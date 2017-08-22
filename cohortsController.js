bf989d3956d04c302b13ea658365f63dbf0bda0c
Jeremy Gray <jeremygray001@gmail.com>
2017-08-22 09:19:16 -0600

    Basic server; getAll method getting cohort IDs that start tomorrow

--- cohortsController.js
+++ cohortsController.js
@@ -10,12 +10,12 @@ module.exports = {
       let now = new Date('2017-11-26T05:00:00.000Z')
       let matchDate = new Date(now.setTime( now.getTime() + 1 * 86400000))
 
-      let cohortNames = data.filter( cohort => {
+      let cohortIDList = data.filter( cohort => {
         let formatDate = new Date(cohort.date_start)
         return cohort.name === 'Web Dev Immersive' && moment(cohort.date_start).format('YYYY-MM-DD') == moment(matchDate).format('YYYY-MM-DD')
       }).map( cohort => { return cohort.id })
       
-      resp.send(cohortNames)
+      resp.send(cohortIDList)
 
     })
 
